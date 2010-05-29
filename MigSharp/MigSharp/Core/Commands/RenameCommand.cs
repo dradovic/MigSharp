a917cdcd3a76c05075db733e809760705cdf65ad
Dejan Radovic <dejan.radovic@gmail.com>
2010-05-29 20:19:54 +0200

    better argument checking in RenameCommand

--- MigSharp/MigSharp/Core/Commands/RenameCommand.cs
+++ MigSharp/MigSharp/Core/Commands/RenameCommand.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 using MigSharp.Providers;
 
@@ -18,21 +17,17 @@ namespace MigSharp.Core.Commands
         public IEnumerable<string> Script(IProvider provider, ICommand parentCommand)
         {
             if (parentCommand == null) throw new ArgumentNullException("parentCommand");
+            AlterTableCommand parentAlterTableCommand = parentCommand as AlterTableCommand;
+            AlterColumnCommand parentAlterColumnCommand = parentCommand as AlterColumnCommand;
+            if (parentAlterTableCommand == null && parentAlterColumnCommand == null) throw new ArgumentException("The parent command of a RenameNode should either be a alterColumnCommand or a alterTableCommand.");
 
-            AlterTableCommand parentAlterTableCommand;
-            AlterColumnCommand parentAlterColumnCommand;
-            if ((parentAlterTableCommand = parentCommand as AlterTableCommand) != null)
+            if ((parentAlterTableCommand) != null)
             {
                 return provider.RenameTable(parentAlterTableCommand.TableName, _newName);
             }
-            else if ((parentAlterColumnCommand = parentCommand as AlterColumnCommand) != null)
-            {
-                return provider.RenameColumn(parentAlterColumnCommand.ColumnName, _newName);
-            }
             else
             {
-                Debug.Assert(false, "The parent command of a RenameNode should either be a alterColumnCommand or a alterTableCommand."); // TODO: make this as an argument check
-                return new string[] { };
+                return provider.RenameColumn(parentAlterColumnCommand.ColumnName, _newName);
             }
         }
     }
