68b0b18f9405a2da2e449f2cf46431ab9a8932b4
Dejan Radovic <dejan.radovic@gmail.com>
2010-05-29 20:09:04 +0200

    first draft how to write a provider based on SMO

---
+++ MigSharp/MigSharp.NUnit/Provider/SqlServerProviderVsSmoTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Reflection;
+
+using Microsoft.SqlServer.Management.Smo;
+
+using NUnit.Framework;
+
+namespace MigSharp.NUnit.Provider
+{
+    [TestFixture]
+    public class SqlServerProviderVsSmoTests
+    {
+        [Test, Explicit]
+        public void TestCreateTable()
+        {
+            Server server = new Server();
+            Database database = new Database(server, "MyDatabase");
+            Table table = new Table(database, "MyTable");
+           
+            //table.Rename("neasdfaw");
+            MethodInfo scriptRename = table.GetType().GetMethod("ScriptRename", BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.IsNotNull(scriptRename);
+            StringCollection query = new StringCollection();
+            ScriptingOptions options = new ScriptingOptions();
+            scriptRename.Invoke(table, new object[] { query, options, "NewName" });
+            foreach (string s in query)
+            {
+                Trace.WriteLine(s);
+            }
+            //Column column = new Column(table, "MyTable")
+            //{
+            //    DataType = DataType.BigInt
+            //};
+            //table.Columns.Add(column);
+            //foreach (string s in table.Script())
+            //{
+            //    Trace.WriteLine(s);
+            //}
+        }
+    }
+}
