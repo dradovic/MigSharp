6efc0488251a13412c4e2195a82d47318c584d55
dradovic <dejan.radovic@gmail.com>
2011-02-17 08:47:18 +0100

    incremented version

--- MigSharp/trunk/MigSharp/Properties/AssemblyInfo.cs
+++ MigSharp/trunk/MigSharp/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MigSharp")]
 [assembly: AssemblyProduct("MigSharp")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 
 [assembly: CLSCompliant(true)]
 
