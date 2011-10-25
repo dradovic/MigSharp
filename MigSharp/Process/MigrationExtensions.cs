effa115bbae152214fb0024b8a2036d92e88241a
unknown <Andreas Knudsen@.(none)>
2011-10-25 13:44:46 +0200

    Added tests for fancy migration timestamp definition

--- MigSharp/Process/MigrationExtensions.cs
+++ MigSharp/Process/MigrationExtensions.cs
@@ -10,10 +10,13 @@ namespace MigSharp.Process
         {
             if (migration == null) throw new ArgumentNullException("migration");
 
-            Match match = Regex.Match(migration.Name, @"^M_(\d+)_\D+");
+            Match match = Regex.Match(migration.Name, @"^M_([\d_]+)_\D.+");
+            if (!match.Success)
+                match = Regex.Match(migration.Name, @"(\d+)$");
             if (!match.Success)
             {
-                throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, "Could not derive timestamp from type name ({0}). Types implementing migrations must be on the format M_<timestamp_with_underscores_<Description>, for example M_1_AddUsers or M_2011_10_08_1340_AddUsers", migration.Name));
+                
+                throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, "Could not derive timestamp from type name ({0}). Types implementing migrations must either be postfixed with a number or be on the format M_<timestamp_with_underscores_<Description>, for example M_1_AddUsers or M_2011_10_08_1340_AddUsers", migration.Name));
             }
             return long.Parse(match.Groups[1].Value.Replace("_",""), CultureInfo.InvariantCulture);
         }
