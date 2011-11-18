ef0cd25eb8cf35695b8c87a0255efb19b854f6d7
unknown <Andreas Knudsen@.(none)>
2011-11-18 21:45:36 +0100

    Added support for custom timestamp naming patterns through a new interface. Also adjusted NUnit tests to reflect this change

---
+++ MigSharp.NUnit/Integration/CustomPatternMigrationBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MigSharp.NUnit.Integration
+{
+    internal abstract class CustomPatternMigrationBase : IMigration, ICustomTimestampPattern
+    {
+        public abstract void Up(IDatabase db);
+
+        public Func<string, long> GetTimestampParser()
+        {
+            return (typeName) =>
+            {
+                Match match = Regex.Match(typeName, @"^M_([\d_]+)_\D.+");
+                if (match.Success)
+                    return long.Parse(match.Groups[1].Value.Replace("_", ""), CultureInfo.InvariantCulture);
+                else
+                    throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, "Could not derive timestamp from type name ({0}). Types implementing migrations must either be postfixed with a number or be on the format M_<timestamp_with_underscores_<Description>, for example M_1_AddUsers or M_2011_10_08_1340_AddUsers", typeName));
+            };
+        }
+    }
+}
---
+++ MigSharp/ICustomTimestampPattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MigSharp
+{
+    /// <summary>
+    /// By implementing this interface in your Migrations you are able to specify 
+    /// how MigSharp should parse your type names into timestamps
+    /// </summary>
+    public interface ICustomTimestampPattern
+    {
+        /// <summary>
+        /// A Func that takes a typename (the name of a Migration class) and returns the timestamp which is encoded in that typename.
+        /// </summary>
+        /// <returns></returns>
+         Func<string, long> GetTimestampParser();
+    }
+}
